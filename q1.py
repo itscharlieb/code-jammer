2b4e741f990c6b9629ffd2075baf00ec5cb9a501
clareification <justclarifying@gmail.com>
2015-11-21 09:53:55 -0500

    save weird version

--- q1.py
+++ q1.py
@@ -2,7 +2,7 @@ import csv
 from sklearn import svm
 import numpy as np
 print(__doc__)
-
+import pickle
 import numpy as np
 import matplotlib.pyplot as plt
 from sklearn import svm, datasets
@@ -20,17 +20,15 @@ labels = []
 for row in d:
 	#print d[row][10]
 	labels.append(d[row][265])
-	features.append(d[row][0:2])
+	features.append(d[row][0:3])
 labels = [int('C' in label) for label in labels]
 for feature in features:
 	feature[0] = int('F' in feature[0])
 	feature[1] = float(feature[1])/10
+    feature[2] = float(feature[2])*1
+
 print features
-# import some data to play with
-iris = datasets.load_iris()
-X = iris.data[:, :2]  # we only take the first two features. We could
-                      # avoid this ugly slicing by using a two-dim dataset
-y = iris.target
+
 X = features
 y = labels
 h = .02  # step size in the mesh
@@ -43,38 +41,3 @@ svc = svm.SVC(kernel='linear', C=C).fit(X, y)
 rbf_svc = svm.SVC(kernel='rbf', gamma=0.7, C=C).fit(X, y)
 poly_svc = svm.SVC(kernel='poly', degree=3, C=C).fit(X, y)
 lin_svc = svm.LinearSVC(C=C).fit(X, y)
-
-# create a mesh to plot in
-x_min, x_max = -1, 2
-y_min, y_max = 0, 10
-xx, yy = np.meshgrid(np.arange(x_min, x_max, h),
-                     np.arange(y_min, y_max, h))
-
-# title for the plots
-titles = ['SVC with linear kernel',
-          'LinearSVC (linear kernel)',
-          'SVC with RBF kernel',
-          'SVC with polynomial (degree 3) kernel']
-for i, clf in enumerate((svc, lin_svc, rbf_svc, poly_svc)):
-    # Plot the decision boundary. For that, we will assign a color to each
-    # point in the mesh [x_min, m_max]x[y_min, y_max].
-    plt.subplot(2, 2, i + 1)
-    plt.subplots_adjust(wspace=0.4, hspace=0.4)
-
-    Z = clf.predict(np.c_[xx.ravel(), yy.ravel()])
-
-    # Put the result into a color plot
-    Z = Z.reshape(xx.shape)
-    plt.contourf(xx, yy, Z, cmap=plt.cm.Paired, alpha=0.8)
-
-    # Plot also the training points
-    plt.scatter(X_1, X_2, c=y, cmap=plt.cm.Paired)
-    plt.xlabel('Sepal length')
-    plt.ylabel('Sepal width')
-    plt.xlim(xx.min(), xx.max())
-    plt.ylim(yy.min(), yy.max())
-    plt.xticks(())
-    plt.yticks(())
-    plt.title(titles[i])
-
-plt.show()
